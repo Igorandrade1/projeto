912aa75b66d5bb27e192d1ac6f531c22ba3ae570
igorandrade1 <igor.freitas2999@hotmail.com>
2022-12-08 19:31:09 -0300

    deleted contact and about

--- recipes/views.py
+++ recipes/views.py
@@ -6,11 +6,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Igor Andrade'
     })
-
-
-def contact(request):
-    return render(request, 'temp/temp.html')
-
-
-def about(request):
-    return HttpResponse('about page')
