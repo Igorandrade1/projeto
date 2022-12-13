fad49f7522f36587e5a7ff43f05fbe17b7e7d8da
igorandrade1 <igor.freitas2999@hotmail.com>
2022-12-13 19:47:30 -0300

    create  url for views.recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,14 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
+        'name': 'Igor Andrade'
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
         'name': 'Igor Andrade'
     })
