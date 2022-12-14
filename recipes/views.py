e11f9e2d3e7cd1a340898b46286d7d7d8aad7b53
igorandrade1 <igor.freitas2999@hotmail.com>
2022-12-13 22:03:29 -0300

    modified view.home add recipe list

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,16 @@
 
 from django.shortcuts import render
 
+from utils.factory import make_recipe
+
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Igor Andrade'
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Igor Andrade'
+        'recipe': make_recipe
     })
